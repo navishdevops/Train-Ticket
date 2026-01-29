4d18c05bf8498afd94cddb7fd8d09fe4fc1c464d
jagadishk07 <jagadishkadiri07@gmail.com>
2026-01-29 22:59:30 +0530

    Create TrainBookApplication.java

---
+++ src/com/shashi/TrainBookApplication.java
@@ -0,0 +1,11 @@
+package com.shashi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainBookApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TrainBookApplication.class, args);
+    }
+}
